049b5935cfd85654ceeaabad5f1bcbab78151c3c
Demian <53890358+demian35@users.noreply.github.com>
2023-07-29 16:30:32 -0600

    controlador para registrar empleados

---
+++ controllers/EmpleadoController.php
@@ -0,0 +1,20 @@
+<?php
+
+class EmpleadosController{
+    private $empleado;
+    //metodo constructor para nuestro controlador
+    public function __construct(){
+        include("../models/Empleado.php");
+        $this->empleado= new Empleado();//creamos un empleado
+    }
+
+    //metodo para guardar los datos en la base de datosS
+    public function guardar($nombre,$apellidos,$matricula,$correo){
+        $id=$this->empleado->insertar($nombre,$apellidos,$matricula,$correo);
+        return ($id!=false)?header("Location:show.php?idEmpleados=".$id):header("Location:agregar.php");
+
+    }
+    
+}
+
+?>
