c613ce790bf5987da882b7dcd2725916837382f6
Demian <53890358+demian35@users.noreply.github.com>
2023-07-29 15:33:03 -0600

    Creando la cabecera de la pagina

---
+++ views/templates/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Administracion de empleados</title>
+    <link rel="stylesheet" href="../../css/bootstrap.min.css" />
+</head>
+
+<body>
+
+    <div class="container-fluid">
+
+        <nav class="navbar navbar-expand navbar-dark bg-primary">
+            <div class="container-fluid">
+
+
+                <ul class="nav navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="../../CerrarSesion.php">Cerrar Sesion </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../crud/index.php">Inicio</a>
+                    </li>
+
+
+                </ul>
+            </div>
+        </nav>
+    </div>
+    <div class="container">
+        <br>
+        <div class="row">
