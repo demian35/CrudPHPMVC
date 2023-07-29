16fa33779ce2d47227ecd9f33baaaad1d8683638
Demian <53890358+demian35@users.noreply.github.com>
2023-07-29 15:45:07 -0600

    Creando el formulario del agregar

---
+++ views/crud/agregar.php
@@ -0,0 +1,52 @@
+<?php
+include("../templates/header.php")
+?>
+
+<style>
+    .card{
+        color: white;
+    }
+</style>
+
+<div class="col-md-12">
+    <div class="card ">
+        <div class="card-header">
+            Registra Empleado
+        </div>
+        <div class="card-body">
+            <form action="../crud/store.php" method="POST">
+
+                <div class="form-group">
+                    <label for="nombre">Nombre</label>
+                    <input required type="text" class="form-control" name="nombre" value="" id="nombre" placeholder="Nombre">
+                </div>
+
+                <div class="form-group">
+                    <label for="apellidos">Apellidos</label>
+                    <input required type="text" class="form-control" name="apellidos" value="" id="apellidos" placeholder="Apellidos">
+                </div>
+
+                <div class="form-group">
+                    <label for="matricula">Matricula</label>
+                    <input required type="text" class="form-control" name="matricula" value="" id="matricula" placeholder="Matricula">
+                </div>
+
+                <div class="form-group">
+                    <label for="correo">Correo Electronico</label>
+                    <input required type="text" class="form-control" name="correo" value="" id="correo" placeholder="Correo Electronico">
+                </div>
+
+                <br>
+
+
+                <br>
+                    <button type="submit" class="btn btn-info">Aceptar</button>
+                    <a class="btn btn-danger" href="../crud/index.php">Cancelar</a>
+            </form>
+
+
+
+
+<?php
+include("../templates/footer.php")
+?>
