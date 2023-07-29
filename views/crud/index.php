3414cea86a3dae9e8656a57faedd021703a785b5
Demian <53890358+demian35@users.noreply.github.com>
2023-07-29 15:43:47 -0600

    Creando la vista principal

---
+++ views/crud/index.php
@@ -0,0 +1,53 @@
+<?php
+include("../templates/header.php")
+?>
+
+
+    <style>
+        .center-screen {
+            text-align: center;
+        }
+        h1{
+            text-align: center;
+            color: white;
+        }
+    </style>
+
+
+    <h1>Registra empleado</h1>
+    <br>
+    <div class="center-screen">
+        <a name="" id="" class="btn btn-primary" href="../crud/agregar.php" role="button">Agregar</a>
+    </div>
+
+    <br>
+
+    <div class="col-md-12 ">
+        <div class="table-responsive table-bordered">
+            <table class="table table-primary">
+                <thead>
+                    <tr>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Apellido</th>
+                        <th scope="col">Matricula</th>
+                        <th scope="col">Correo Electronico</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="">
+                        <td scope="row">Fulanito</td>
+                        <td>Perez Salgado</td>
+                        <td>R1C3</td>
+                        <td>ful@correo.com</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+
+
+
+<?php
+include("../templates/footer.php")
+?>
