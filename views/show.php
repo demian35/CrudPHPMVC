58560db4cf8ad5e7174c3d59c790f5fa78d2702a
Demian <53890358+demian35@users.noreply.github.com>
2023-07-31 03:41:32 -0600

    Terminando vista para lectura de registro

--- views/show.php
+++ views/show.php
@@ -4,13 +4,64 @@ include("./templates/header.php")
 
 <?php
 include("../controllers/EmpleadoController.php");
-$empleado=new EmpleadoController;
+$empleado = new EmpleadoController;
 
-print_r($empleado->muestraRegistro($_GET['idEmpleados']));
+$resultado = $empleado->muestraRegistro($_GET['idEmpleados']);
 
 ?>
 
+<style>
+    .center-screen {
+        text-align: center;
+    }
+
+    h1 {
+        text-align: center;
+        color: white;
+    }
+</style>
+
 <h1 class="text-center">Empleado</h1>
+<div class="center-screen">
+    <a class="btn btn-primary" href="index.php" role="button">Regresar a inicio</a>
+</div>
+
+
+
+
+
+<div class="col-md-12 ">
+    <br>
+    <div class="table-responsive table-bordered">
+        <table class="table table-primary">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Apellido</th>
+                    <th scope="col">Matricula</th>
+                    <th scope="col">Correo Electronico</th>
+                    <th scope="col">acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+               
+                    <tr class="">
+                        <td> <?php echo $resultado[0] ?></td>
+                        <td scope="row"><?php echo $resultado[1] ?></td>
+                        <td><?php echo $resultado[2] ?></td>
+                        <td><?php echo $resultado[3] ?></td>
+                        <td><?php echo $resultado[4] ?></td>
+                        <td>
+                           
+                        </td>
+                    </tr>
+                
+            </tbody>
+        </table>
+    </div>
+</div>
+
 
 
 <?php
