a19555f899482d4e432e04a9a93ad7acf87d1c74
Demian <53890358+demian35@users.noreply.github.com>
2023-07-31 02:28:42 -0600

    Poniendo los botones para eliminar y editar

--- views/index.php
+++ views/index.php
@@ -5,58 +5,65 @@ include("./templates/header.php")
 <?php
 include("../controllers/EmpleadoController.php");
 
-$empleado=new EmpleadoController;
-$resultados=$empleado->muestra();//mostramos los resultados
-
+$empleado = new EmpleadoController;
+$resultados = $empleado->muestra(); //mostramos los resultados
 
 ?>
 
 
-    <style>
-        .center-screen {
-            text-align: center;
-        }
-        h1{
-            text-align: center;
-            color: white;
-        }
-    </style>
+<style>
+    .center-screen {
+        text-align: center;
+    }
+
+    h1 {
+        text-align: center;
+        color: white;
+    }
+</style>
 
 
-    <h1>Registra empleado</h1>
+<h1>Registra empleado</h1>
+<br>
+<div class="center-screen">
+    <a class="btn btn-primary" href="agregar.php" role="button">Agregar</a>
+</div>
+
+
+
+
+<div class="col-md-12 ">
     <br>
-    <div class="center-screen">
-        <a class="btn btn-primary" href="agregar.php" role="button">Agregar</a>
-    </div>
-
-    <br>
-
-    <div class="col-md-12 ">
-        <div class="table-responsive table-bordered">
-            <table class="table table-primary">
-                <thead>
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Nombre</th>
-                        <th scope="col">Apellido</th>
-                        <th scope="col">Matricula</th>
-                        <th scope="col">Correo Electronico</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach($resultados as $empleados) { ?>
+    <div class="table-responsive table-bordered">
+        <table class="table table-primary">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Apellido</th>
+                    <th scope="col">Matricula</th>
+                    <th scope="col">Correo Electronico</th>
+                    <th scope="col">acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($resultados as $empleados) { ?>
                     <tr class="">
                         <td> <?php echo $empleados[0] ?></td>
                         <td scope="row"><?php echo $empleados[1] ?></td>
                         <td><?php echo $empleados[2] ?></td>
                         <td><?php echo $empleados[3] ?></td>
                         <td><?php echo $empleados[4] ?></td>
+                        <td>
+                            <a class="btn btn-danger" href="delete.php?idEmpleados=<?php $empleados[0]; ?>" role="button">Eliminar</a>
+                            <a class="btn btn-info" href="editar.php?idEmpleados=" <?php $resultados[0]; ?> role="button">Editar</a>
+                        </td>
                     </tr>
-                    <?php } ?>
-                </tbody>
-            </table>
-        </div>
+                <?php } ?>
+            </tbody>
+        </table>
     </div>
+</div>
 
 
 
