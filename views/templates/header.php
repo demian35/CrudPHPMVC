086b7ff218edad547b03b8364a8684cb1bed665a
Demian <53890358+demian35@users.noreply.github.com>
2023-08-01 01:11:43 -0600

    linea para cambiar de titulos en la navegacion

--- views/templates/header.php
+++ views/templates/header.php
@@ -4,8 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Administracion de empleados</title>
     <link rel="stylesheet" href="../../css/bootstrap.min.css" />
+    <title>
+        <?php 
+        echo (empty($_GET['idEmpleados']))?(((strpos($_SERVER['REQUEST_URI'],'agregar'))?"Agrega Usuario":"Inicio")):((strpos($_SERVER['REQUEST_URI'],'show'))?"Detalles empleado".$_GET['idEmpleados']:"Editar Registro".$_GET['idEmpleados']);
+
+        ?>
+
+    </title>
 </head>
 
 <body>
