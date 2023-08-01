8e21964831774aa90a794e3b613798e1e8df807f
Demian <53890358+demian35@users.noreply.github.com>
2023-07-31 18:42:57 -0600

    boton borrar y notificacion de borrado de registro

--- views/show.php
+++ views/show.php
@@ -24,28 +24,42 @@ $resultado = $empleado->muestraRegistro($_GET['idEmpleados']);
 <h1 class="text-center">Empleado</h1>
 <div class="center-screen">
     <a class="btn btn-primary" href="index.php" role="button">Regresar a inicio</a>
-</div>
+    <a class="btn btn-info" href="editar.php?idEmpleados=<?php $resultado[0] ?>" role="button">Editar</a>
+    <a class="btn btn-danger" id="delete-btn" href="#" role="button">Borrar</a>
+    <script>
+        document.getElementById("delete-btn").addEventListener("click", function(event) {
+            event.preventDefault(); // Evitar que el enlace se ejecute de inmediato
+
+            // Mostrar una alerta para confirmar la eliminación
+            if (confirm("¿Estás seguro de que deseas eliminar a este empleado?")) {
+                // Si el usuario confirma, redirige a la página o ejecuta la lógica para eliminar el registro
+                // Por ejemplo, puedes usar window.location.href para redirigir a otra página:
+                // window.location.href = "ruta_para_eliminar_registro.php";
+            }
+        });
+    </script>
 
 
 
 
 
-<div class="col-md-12 ">
-    <br>
-    <div class="table-responsive table-bordered">
-        <table class="table table-primary">
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Nombre</th>
-                    <th scope="col">Apellido</th>
-                    <th scope="col">Matricula</th>
-                    <th scope="col">Correo Electronico</th>
-                    <th scope="col">acciones</th>
-                </tr>
-            </thead>
-            <tbody>
-               
+
+    <div class="col-md-12 ">
+        <br>
+        <div class="table-responsive table-bordered">
+            <table class="table table-primary">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Apellido</th>
+                        <th scope="col">Matricula</th>
+                        <th scope="col">Correo Electronico</th>
+                        <th scope="col">acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+
                     <tr class="">
                         <td> <?php echo $resultado[0] ?></td>
                         <td scope="row"><?php echo $resultado[1] ?></td>
@@ -53,17 +67,17 @@ $resultado = $empleado->muestraRegistro($_GET['idEmpleados']);
                         <td><?php echo $resultado[3] ?></td>
                         <td><?php echo $resultado[4] ?></td>
                         <td>
-                           
+
                         </td>
                     </tr>
-                
-            </tbody>
-        </table>
+
+                </tbody>
+            </table>
+        </div>
     </div>
-</div>
 
 
 
-<?php
-include("./templates/footer.php")
-?>
+    <?php
+    include("./templates/footer.php")
+    ?>
