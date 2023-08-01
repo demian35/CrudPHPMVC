0ec3b06024afc7d5a1180e1395af00637b45acc9
Demian <53890358+demian35@users.noreply.github.com>
2023-08-01 02:42:24 -0600

    validacion de datos para el login con la bd

--- controladorlogin.php
+++ controladorlogin.php
@@ -3,11 +3,26 @@
 
 <?php
 
-//si se presiona el boton de iniciar sesion
-if(!empty($_POST['btningresar']))
-    if(empty($_POST['usuario']) and empty($_POST['contrasena']) ){//si los campos estan vacios mandamos la alerta
-        $notificacion= "Por favor rellena los campos";
-    }else{
+include_once("./config/conexionBD.php");
+$bd=new conexionBD;
 
+if(!empty($_POST['btningresar']))
+if(empty($_POST['usuario']) and empty($_POST['contrasena']) ){//si los campos estan vacios mandamos la alerta
+    $notificacion= "Por favor rellena los campos";
+}else{
+    $usuario=$_POST['usuario'];
+    $contrasena=$_POST['contrasena'];
+    
+    //verificamos que los datos que mande el usuario esten e registrados
+    $conexion=$bd->conexion();
+    $consulta=$conexion->prepare("SELECT * FROM usuarios WHERE usuario=:usuario AND contrasena=:contrasena ");
+    $consulta->bindParam(':usuario',$usuario);
+    $consulta->bindParam(':contrasena',$contrasena);
+    $consulta->execute();
+    if($datos=$consulta->fetchObject()){
+        header("Location: /views/index.php");
+    }else{
+        $mensaje="Datos incorrectos";
     }
-?>
+    
+}
