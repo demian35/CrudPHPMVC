f3b9b62ab7e7414d6a9eadbbcbb66d6c116ea867
Demian <53890358+demian35@users.noreply.github.com>
2023-07-31 22:59:32 -0600

    leyenda que no hay nada registrado y botones reparados

--- views/index.php
+++ views/index.php
@@ -15,6 +15,10 @@ $resultados = $empleado->muestra(); //mostramos los resultados
     .center-screen {
         text-align: center;
     }
+    
+    .text-center{
+        text-align: center;
+    }
 
     h1 {
         text-align: center;
@@ -46,34 +50,50 @@ $resultados = $empleado->muestra(); //mostramos los resultados
                     <th scope="col">acciones</th>
                 </tr>
             </thead>
-            <tbody>
-                <?php foreach ($resultados as $empleados) { ?>
-                    <tr class="">
-                        <td> <?= $empleados['idEmpleados'] ?></td>
-                        <td scope="row"><?= $empleados['nombre'] ?></td>
-                        <td><?= $empleados['apellidos'] ?></td>
-                        <td><?= $empleados['matricula'] ?></td>
-                        <td><?= $empleados['correo'] ?></td>
-                        <td>
-                            <a class="btn btn-secondary" href="show.php?idEmpleados=<?= $empleados['idEmpleados']; ?>" role="button">Ver Empleado</a>
-                            <a class="btn btn-success" href="editar.php?idEmpleados=<?= $empleados['idEmpleados']; ?>" role="button">Editar</a>
-                            <a class="btn btn-danger"  id="delete-btn" href="#" role="button">Borrar</a>
-                            <script>
-                                document.getElementById("delete-btn").addEventListener("click", function(event) {
-                                    event.preventDefault(); // Evitar que el enlace se ejecute de inmediato
+            <?php if ($resultados) : ?>
+                <tbody>
+                    <?php foreach ($resultados as $empleados) { ?>
+                        <tr class="">
+                            <td> <?= $empleados['idEmpleados'] ?></td>
+                            <td scope="row"><?= $empleados['nombre'] ?></td>
+                            <td><?= $empleados['apellidos'] ?></td>
+                            <td><?= $empleados['matricula'] ?></td>
+                            <td><?= $empleados['correo'] ?></td>
+                            <td>
+                                <a class="btn btn-secondary" href="show.php?idEmpleados=<?= $empleados['idEmpleados']; ?>" role="button">Ver Empleado</a>
+                                <a class="btn btn-success" href="editar.php?idEmpleados=<?= $empleados['idEmpleados']; ?>" role="button">Editar</a>
+                                <a class="btn btn-danger" id="delete-btn" href="delete.php?idEmpleados=<?= $empleados['idEmpleados']; ?>" role="button">Borrar</a>
 
-                                    // Mostrar una alerta para confirmar la eliminación
-                                    if (confirm("¿Estás seguro de que deseas eliminar a este empleado?")) {
-                                        // Si el usuario confirma, redirige a la página o ejecuta la lógica para eliminar el registro
+                            </td>
+                        </tr> <!-- Script para el manejo de clics en los botones de borrado -->
+                    <?php } ?>
+                </tbody>
+                <!-- Script para el manejo de clics en los botones de borrado -->
+                <script>
+                    const deleteButtons = document.querySelectorAll(".delete-btn");
 
-                                        window.location.href = "delete.php?idEmpleados=<?= $empleados['idEmpleados'] ?>";
-                                    }
-                                });
-                            </script>
-                        </td>
-                    </tr>
-                <?php } ?>
-            </tbody>
+                    deleteButtons.forEach(button => {
+                        button.addEventListener("click", function(event) {
+                            event.preventDefault(); // Evitar que el enlace se ejecute de inmediato
+
+                            // Obtener el ID del empleado a eliminar desde el atributo data
+                            const idEmpleado = button.getAttribute("data-id-empleado");
+
+                            // Mostrar una alerta para confirmar la eliminación
+                            if (confirm("¿Estás seguro de que deseas eliminar a este empleado?")) {
+                                // Si el usuario confirma, redirige a la página o ejecuta la lógica para eliminar el registro
+                                window.location.href = `delete.php?idEmpleados=${idEmpleado}`;
+                            }
+                        });
+                    });
+                </script>
+
+            <?php else : ?>
+                <!-- Si no hay resultados -->
+               <tr>
+                <td colspan="6" class="text-center">No hay empleados registrados</td>
+               </tr>
+            <?php endif; ?>
         </table>
     </div>
 </div>
@@ -82,5 +102,5 @@ $resultados = $empleado->muestra(); //mostramos los resultados
 
 
 <?php
-include("./templates/footer.php")
+include("./templates/footer.php");
 ?>
